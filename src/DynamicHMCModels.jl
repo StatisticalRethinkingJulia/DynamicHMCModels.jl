9cca15ee725ea42ebf843210e53e365c5c48f517
Rob J Goedman <goedman@mac.com>
2019-09-03 07:51:08 -0400

    Removed Flux as a dependency

--- src/DynamicHMCModels.jl
+++ src/DynamicHMCModels.jl
@@ -3,7 +3,7 @@ module DynamicHMCModels
 using Reexport 
 
 @reexport using CSV, LinearAlgebra
-@reexport using ForwardDiff, Flux
+@reexport using ForwardDiff
 @reexport using DynamicHMC, LogDensityProblems, TransformVariables
 @reexport using StatsFuns, Distributions, Random, StatsBase, MCMCChains
 @reexport using Parameters, CSV, DataFrames
@@ -14,9 +14,6 @@ include("chains.jl")
 include("nptochain.jl")
 
 export
-  rel_path_d,
-  script_dict_d,
-  generate_d,
   create_a3d,
   insert_chain!,
   nptochain,
